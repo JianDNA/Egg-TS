ba58d234e341897e3dad59db0422605b55a9de37
HongZJ <vusn@qq.com>
2021-02-09 12:18:31 +0800

    完成sequelize-cli的配置, 编写自定义脚本处理ts的迁移文件

---
+++ database/migrations/20210209041029-users.ts
@@ -0,0 +1,22 @@
+import { QueryInterface } from 'sequelize';
+
+// ts文件需要自定义脚本执行ts文件
+// https://blog.csdn.net/taokexia/article/details/105556703
+// "sequelize-cli-ts": "node -r ts-node/register ./node_modules/sequelize-cli/lib/sequelize"
+module.exports = {
+  // 在执行数据库升级时调用的函数，创建 users 表
+  up: async (queryInterface: QueryInterface, Sequelize) => {
+    const { INTEGER, DATE, STRING } = Sequelize;
+    await queryInterface.createTable('users', {
+      id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+      name: STRING(30),
+      age: INTEGER,
+      created_at: DATE,
+      updated_at: DATE,
+    });
+  },
+  // 在执行数据库降级时调用的函数，删除 users 表
+  down: async (queryInterface : QueryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
